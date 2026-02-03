1748e25e264a1229710377ec0f8a335d2ecbbc0c
vaibhavkale007 <vaibhavskale007@gmail.com>
2026-02-03 08:38:53 -0700

    Change API endpoint from register to login

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -50,7 +50,7 @@ const Signup = () => {
     try {
       dispatch(setLoading(true));
 
-      const res = await api.post("/api/v1/user/register", formData, {
+      const res = await api.post("/api/v1/user/login", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
